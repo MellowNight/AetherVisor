13275ec7f0590d71354763d13c88a926a95c686c
MellowNight <89277220+MellowNight@users.noreply.github.com>
2022-03-05 09:59:04 -0500

    asdasd

--- AMD-Hypervisor/prepare_vm.cpp
+++ AMD-Hypervisor/prepare_vm.cpp
@@ -150,210 +150,6 @@ SegmentAttribute GetSegmentAttributes(uint16_t segment_selector, uintptr_t gdt_b
 	return attribute;
 }
 
-typedef struct
-{
-	/**
-	 * @brief Segment limit field (15:00)
-	 *
-	 * Specifies the size of the segment. The processor puts together the two segment limit fields to form a 20-bit value. The
-	 * processor interprets the segment limit in one of two ways, depending on the setting of the G (granularity) flag:
-	 * - If the granularity flag is clear, the segment size can range from 1 byte to 1 MByte, in byte increments.
-	 * - If the granularity flag is set, the segment size can range from 4 KBytes to 4 GBytes, in 4-KByte increments.
-	 * The processor uses the segment limit in two different ways, depending on whether the segment is an expand-up or an
-	 * expand-down segment. For expand-up segments, the offset in a logical address can range from 0 to the segment limit.
-	 * Offsets greater than the segment limit generate general-protection exceptions (\#GP, for all segments other than SS) or
-	 * stack-fault exceptions (\#SS for the SS segment). For expand-down segments, the segment limit has the reverse function;
-	 * the offset can range from the segment limit plus 1 to FFFFFFFFH or FFFFH, depending on the setting of the B flag.
-	 * Offsets less than or equal to the segment limit generate general-protection exceptions or stack-fault exceptions.
-	 * Decreasing the value in the segment limit field for an expanddown segment allocates new memory at the bottom of the
-	 * segment's address space, rather than at the top. IA-32 architecture stacks always grow downwards, making this mechanism
-	 * convenient for expandable stacks.
-	 *
-	 * @see Vol3A[3.4.5.1(Code- and Data-Segment Descriptor Types)]
-	 */
-	uint16_t segment_limit_low;
-
-	/**
-	 * @brief Base address field (15:00)
-	 *
-	 * Defines the location of byte 0 of the segment within the 4-GByte linear address space. The processor puts together the
-	 * three base address fields to form a single 32-bit value. Segment base addresses should be aligned to 16-byte boundaries.
-	 * Although 16-byte alignment is not required, this alignment allows programs to maximize performance by aligning code and
-	 * data on 16-byte boundaries.
-	 */
-	uint16_t base_address_low;
-	/**
-	 * @brief Segment descriptor fields
-	 */
-	union
-	{
-		struct
-		{
-			/**
-			 * [Bits 7:0] Base address field (23:16); see description of $BASE_LOW for more details.
-			 */
-			uint32_t base_address_middle : 8;
-#define SEGMENT__BASE_ADDRESS_MIDDLE_BIT                             16
-#define SEGMENT__BASE_ADDRESS_MIDDLE_FLAG                            0xFF
-#define SEGMENT__BASE_ADDRESS_MIDDLE_MASK                            0xFF
-#define SEGMENT__BASE_ADDRESS_MIDDLE(_)                              (((_) >> 0) & 0xFF)
-
-			/**
-			 * @brief Type field
-			 *
-			 * [Bits 11:8] Indicates the segment or gate type and specifies the kinds of access that can be made to the segment and the
-			 * direction of growth. The interpretation of this field depends on whether the descriptor type flag specifies an
-			 * application (code or data) descriptor or a system descriptor. The encoding of the type field is different for code,
-			 * data, and system descriptors.
-			 *
-			 * @see Vol3A[3.4.5.1(Code- and Data-Segment Descriptor Types)]
-			 */
-			uint32_t type : 4;
-#define SEGMENT__TYPE_BIT                                            8
-#define SEGMENT__TYPE_FLAG                                           0xF00
-#define SEGMENT__TYPE_MASK                                           0x0F
-#define SEGMENT__TYPE(_)                                             (((_) >> 8) & 0x0F)
-
-			/**
-			 * @brief S (descriptor type) flag
-			 *
-			 * [Bit 12] Specifies whether the segment descriptor is for a system segment (S flag is clear) or a code or data segment (S
-			 * flag is set).
-			 */
-			uint32_t descriptor_type : 1;
-#define SEGMENT__DESCRIPTOR_TYPE_BIT                                 12
-#define SEGMENT__DESCRIPTOR_TYPE_FLAG                                0x1000
-#define SEGMENT__DESCRIPTOR_TYPE_MASK                                0x01
-#define SEGMENT__DESCRIPTOR_TYPE(_)                                  (((_) >> 12) & 0x01)
-
-			/**
-			 * @brief DPL (descriptor privilege level) field
-			 *
-			 * [Bits 14:13] Specifies the privilege level of the segment. The privilege level can range from 0 to 3, with 0 being the
-			 * most privileged level. The DPL is used to control access to the segment. See Section 5.5, "Privilege Levels", for a
-			 * description of the relationship of the DPL to the CPL of the executing code segment and the RPL of a segment selector.
-			 */
-			uint32_t descriptor_privilege_level : 2;
-#define SEGMENT__DESCRIPTOR_PRIVILEGE_LEVEL_BIT                      13
-#define SEGMENT__DESCRIPTOR_PRIVILEGE_LEVEL_FLAG                     0x6000
-#define SEGMENT__DESCRIPTOR_PRIVILEGE_LEVEL_MASK                     0x03
-#define SEGMENT__DESCRIPTOR_PRIVILEGE_LEVEL(_)                       (((_) >> 13) & 0x03)
-
-			/**
-			 * @brief P (segment-present) flag
-			 *
-			 * [Bit 15] Indicates whether the segment is present in memory (set) or not present (clear). If this flag is clear, the
-			 * processor generates a segment-not-present exception (\#NP) when a segment selector that points to the segment descriptor
-			 * is loaded into a segment register. Memory management software can use this flag to control which segments are actually
-			 * loaded into physical memory at a given time. It offers a control in addition to paging for managing virtual memory.
-			 */
-			uint32_t present : 1;
-#define SEGMENT__PRESENT_BIT                                         15
-#define SEGMENT__PRESENT_FLAG                                        0x8000
-#define SEGMENT__PRESENT_MASK                                        0x01
-#define SEGMENT__PRESENT(_)                                          (((_) >> 15) & 0x01)
-
-			/**
-			 * [Bits 19:16] Segment limit field (19:16); see description of $LIMIT_LOW for more details.
-			 */
-			uint32_t segment_limit_high : 4;
-#define SEGMENT__SEGMENT_LIMIT_HIGH_BIT                              16
-#define SEGMENT__SEGMENT_LIMIT_HIGH_FLAG                             0xF0000
-#define SEGMENT__SEGMENT_LIMIT_HIGH_MASK                             0x0F
-#define SEGMENT__SEGMENT_LIMIT_HIGH(_)                               (((_) >> 16) & 0x0F)
-
-			/**
-			 * @brief Available bit
-			 *
-			 * [Bit 20] Bit 20 of the second doubleword of the segment descriptor is available for use by system software.
-			 */
-			uint32_t system : 1;
-#define SEGMENT__SYSTEM_BIT                                          20
-#define SEGMENT__SYSTEM_FLAG                                         0x100000
-#define SEGMENT__SYSTEM_MASK                                         0x01
-#define SEGMENT__SYSTEM(_)                                           (((_) >> 20) & 0x01)
-
-			/**
-			 * @brief L (64-bit code segment) flag
-			 *
-			 * [Bit 21] In IA-32e mode, bit 21 of the second doubleword of the segment descriptor indicates whether a code segment
-			 * contains native 64-bit code. A value of 1 indicates instructions in this code segment are executed in 64-bit mode. A
-			 * value of 0 indicates the instructions in this code segment are executed in compatibility mode. If L-bit is set, then
-			 * D-bit must be cleared. When not in IA-32e mode or for non-code segments, bit 21 is reserved and should always be set to
-			 * 0.
-			 */
-			uint32_t long_mode : 1;
-#define SEGMENT__LONG_MODE_BIT                                       21
-#define SEGMENT__LONG_MODE_FLAG                                      0x200000
-#define SEGMENT__LONG_MODE_MASK                                      0x01
-#define SEGMENT__LONG_MODE(_)                                        (((_) >> 21) & 0x01)
-
-			/**
-			 * @brief D/B (default operation size/default stack pointer size and/or upper bound) flag
-			 *
-			 * [Bit 22] Performs different functions depending on whether the segment descriptor is an executable code segment, an
-			 * expand-down data segment, or a stack segment. (This flag should always be set to 1 for 32-bit code and data segments and
-			 * to 0 for 16-bit code and data segments.)
-			 * - Executable code segment. The flag is called the D flag and it indicates the default length for effective addresses and
-			 * operands referenced by instructions in the segment. If the flag is set, 32-bit addresses and 32-bit or 8-bit operands
-			 * are assumed; if it is clear, 16-bit addresses and 16-bit or 8-bit operands are assumed. The instruction prefix 66H can
-			 * be used to select an operand size other than the default, and the prefix 67H can be used select an address size other
-			 * than the default.
-			 * - Stack segment (data segment pointed to by the SS register). The flag is called the B (big) flag and it specifies the
-			 * size of the stack pointer used for implicit stack operations (such as pushes, pops, and calls). If the flag is set, a
-			 * 32-bit stack pointer is used, which is stored in the 32-bit ESP register; if the flag is clear, a 16-bit stack pointer
-			 * is used, which is stored in the 16- bit SP register. If the stack segment is set up to be an expand-down data segment
-			 * (described in the next paragraph), the B flag also specifies the upper bound of the stack segment.
-			 * - Expand-down data segment. The flag is called the B flag and it specifies the upper bound of the segment. If the flag
-			 * is set, the upper bound is FFFFFFFFH (4 GBytes); if the flag is clear, the upper bound is FFFFH (64 KBytes).
-			 */
-			uint32_t default_big : 1;
-#define SEGMENT__DEFAULT_BIG_BIT                                     22
-#define SEGMENT__DEFAULT_BIG_FLAG                                    0x400000
-#define SEGMENT__DEFAULT_BIG_MASK                                    0x01
-#define SEGMENT__DEFAULT_BIG(_)                                      (((_) >> 22) & 0x01)
-
-			/**
-			 * @brief G (granularity) flag
-			 *
-			 * [Bit 23] Determines the scaling of the segment limit field. When the granularity flag is clear, the segment limit is
-			 * interpreted in byte units; when flag is set, the segment limit is interpreted in 4-KByte units. (This flag does not
-			 * affect the granularity of the base address; it is always byte granular.) When the granularity flag is set, the twelve
-			 * least significant bits of an offset are not tested when checking the offset against the segment limit. For example, when
-			 * the granularity flag is set, a limit of 0 results in valid offsets from 0 to 4095.
-			 */
-			uint32_t granularity : 1;
-#define SEGMENT__GRANULARITY_BIT                                     23
-#define SEGMENT__GRANULARITY_FLAG                                    0x800000
-#define SEGMENT__GRANULARITY_MASK                                    0x01
-#define SEGMENT__GRANULARITY(_)                                      (((_) >> 23) & 0x01)
-
-			/**
-			 * [Bits 31:24] Base address field (31:24); see description of $BASE_LOW for more details.
-			 */
-			uint32_t base_address_high : 8;
-#define SEGMENT__BASE_ADDRESS_HIGH_BIT                               24
-#define SEGMENT__BASE_ADDRESS_HIGH_FLAG                              0xFF000000
-#define SEGMENT__BASE_ADDRESS_HIGH_MASK                              0xFF
-#define SEGMENT__BASE_ADDRESS_HIGH(_)                                (((_) >> 24) & 0xFF)
-		};
-
-		uint32_t flags;
-	};
-
-
-	/**
-	 * Base address field (32:63); see description of $BASE_LOW for more details.
-	 */
-	uint32_t base_address_upper;
-#define SEGMENT__BASE_ADDRESS_SHIFT 32
-
-	/**
-	 * Base address field (32:63); see description of $BASE_LOW for more details.
-	 */
-	uint32_t must_be_zero;
-} segment_descriptor_64;
-
 void SetupTssIst()
 {	
 	DescriptorTableRegister	gdtr;
@@ -362,7 +158,7 @@ void SetupTssIst()
 	SEGMENT_SELECTOR selector;
 	selector.Flags = __readtr();
 
-	auto seg_descriptor = *reinterpret_cast<segment_descriptor_64*>(
+	auto seg_descriptor = *(SEGMENT_DESCRIPTOR_64*)(
 		gdtr.base + (selector.Index * 8));
 
 	auto base = (uintptr_t)(
@@ -466,13 +262,16 @@ void ConfigureProcessor(CoreVmcbData* core_data, CONTEXT* context_record)
 	intercept_vector2.intercept_bp = 1;
 
 	core_data->guest_vmcb.control_area.InterceptException = intercept_vector2.as_int32;
-
+	
 	core_data->guest_vmcb.control_area.GuestAsid = 1;
+
+	VpData->guest_vmcb.control_area.NCr3 = hypervisor->normal_ncr3;
+	VpData->guest_vmcb.control_area.NpEnable = (1UL << 0);
+
 	core_data->guest_vmcb.save_state_area.Cr0 = __readcr0();
 	core_data->guest_vmcb.save_state_area.Cr2 = __readcr2();
 	core_data->guest_vmcb.save_state_area.Cr3 = __readcr3();
 	core_data->guest_vmcb.save_state_area.Cr4 = __readcr4();
-
 
 	core_data->guest_vmcb.save_state_area.Rip = context_record->Rip;
 	core_data->guest_vmcb.save_state_area.Rax = context_record->Rax;
