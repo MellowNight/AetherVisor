a1eed0108103292f432ffc7a411dd15fac19fbcd
MellowNight <89277220+MellowNight@users.noreply.github.com>
2022-08-08 17:32:48 -0400

    update

--- AMD-Hypervisor/npthook_safety.cpp
+++ AMD-Hypervisor/npthook_safety.cpp
@@ -7,28 +7,7 @@ namespace NptHooks
 	int hook_count;
 	NptHook first_npt_hook;
 
-	Hooks::JmpRipCode hk_NtTerminateProcess;
 	Hooks::JmpRipCode hk_MmCleanProcessAddressSpace;
-
-	__int64 __fastcall NtTerminateProcess_hook(__int64 a1, __int64 a2)
-	{
-		/*	unset all NPT hooks for this process	*/
-
-		ForEachHook(
-			[](NptHook* hook_entry, void* data)->bool {
-
-				if (hook_entry->process_cr3 == (uintptr_t)data)
-				{
-					UnsetHook(hook_entry);
-				}
-
-				return false;
-			}, 
-			(void*)__readcr3()
-		);
-
-		return static_cast<decltype(&NtTerminateProcess_hook)>(hk_NtTerminateProcess.original_bytes)(a1, a2);
-	}
 
 	char __fastcall MmCleanProcessAddressSpace_hook(__int64 a1, __int64 a2)
 	{
@@ -69,20 +48,14 @@ namespace NptHooks
 			{
 				uint8_t* start = section[i].VirtualAddress + (uint8_t*)ntoskrnl;
 
-				uintptr_t terminate_process = NULL;
-				uintptr_t clean_process_address_space = NULL;
+				uintptr_t clean_process_address_space = RELATIVE_ADDR(Utils::FindPattern((uintptr_t)start, section[i].Misc.VirtualSize, "\xE8\xCC\xCC\xCC\xCC\x8B\x5D\x00", 8, 0xCC), 1, 5);
 
-				terminate_process = Utils::FindPattern((uintptr_t)start, section[i].Misc.VirtualSize, "\x4C\x8B\xDC\x49\x89\x5B\x10\x49\x89\x6B\x18\x57", 12, 0x00);
-				clean_process_address_space = RELATIVE_ADDR(Utils::FindPattern((uintptr_t)start, section[i].Misc.VirtualSize, "\xE8\xCC\xCC\xCC\xCC\x8B\x5D\x00", 8, 0xCC), 1, 5);
-
-				hk_NtTerminateProcess = Hooks::JmpRipCode{ terminate_process, (uintptr_t)NtTerminateProcess_hook };
 				hk_MmCleanProcessAddressSpace = Hooks::JmpRipCode{ clean_process_address_space, (uintptr_t)MmCleanProcessAddressSpace_hook };
 
 				LARGE_INTEGER length_tag;
 				length_tag.LowPart = NULL;
-				length_tag.HighPart = hk_NtTerminateProcess.hook_size;
+				length_tag.HighPart = hk_MmCleanProcessAddressSpace.hook_size;
 
-				svm_vmmcall(VMMCALL_ID::set_npt_hook, terminate_process, hk_NtTerminateProcess.hook_code, length_tag.QuadPart);
 				svm_vmmcall(VMMCALL_ID::set_npt_hook, clean_process_address_space, hk_MmCleanProcessAddressSpace.hook_code, length_tag.QuadPart);
 			}
 
